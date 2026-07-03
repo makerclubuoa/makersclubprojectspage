9c252649e19adec6712009651b8f3aa93025048d
zozzzC <czlc05edu@gmail.com>
2026-07-04 11:52:32 +1200

    test: add Makeathon section to test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,22 +1,28 @@
+"use client";
 import Button from "../components/global/Button";
+import Polaroid from "../components/global/Polaroid";
 import Header from "../components/homepage/Header";
+import MakeathonSection from "../components/homepage/MakeathonSection";
 import MovingText from "../components/homepage/MovingText";
 import Splash from "../components/homepage/Splash";
 
+import art from "@/public/maker-club-art.png";
+
 export default function Test() {
   return (
-    <>
+    <div className="overflow-x-hidden">
       <Splash />
-      <div className="relative z-10 pb-1.5">
+      <div className="relative -top-10 lg:-top-7 z-10 pb-1.5">
         <MovingText />
       </div>
       <Header
         text="What's New?"
         rotation={-1.5}
-        typeOverride="h-20 pl-10"
+        typeOverride="relative -top-10 lg:-top-3 h-20 pl-5 md:pl-10"
         bgColour="pop-pink"
         colour="white"
       />
-    </>
+      <MakeathonSection />
+    </div>
   );
 }
