a21e44ea2d4547bcc5e07db8b98e36a1cf30d913
zozzzC <czlc05edu@gmail.com>
2026-07-03 16:08:41 +1200

    chore: add correct fonts and variables into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,25 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import AuthProvider from "@/app/components/AuthProvider";
+import { DM_Sans } from "next/font/google";
+import { Holtwood_One_SC } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "PROJECTS · MAKER CLUB",
   icons: { icon: "/logo.png" },
 };
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dm-sans",
+});
+
+const holtwood = Holtwood_One_SC({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-holtwood",
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${dmSans.variable} ${holtwood.variable}`}>
       <head />
       <body>
         {/* <script dangerouslySetInnerHTML={{ __html: `(function(){var t=localStorage.getItem('theme');var s=window.matchMedia('(prefers-color-scheme: dark)').matches;if(t==='dark'||(t===null&&s)){document.body.dataset.mode='dark';document.documentElement.style.colorScheme='dark';}else{document.body.dataset.mode='light';}})()` }} /> */}
