d32669ea8742d96b82a6cff672ddaa58922d55d6
zozzzC <czlc05edu@gmail.com>
2026-07-03 17:11:25 +1200

    fix: position moving text properly

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,11 +1,15 @@
 import Button from "../components/global/Button";
 import Header from "../components/homepage/Header";
+import MovingText from "../components/homepage/MovingText";
 import Splash from "../components/homepage/Splash";
 
 export default function Test() {
   return (
     <>
       <Splash />
+      <div className="relative z-10 pb-1.5">
+        <MovingText />
+      </div>
       <Header
         text="What's New?"
         rotation={-1.5}
