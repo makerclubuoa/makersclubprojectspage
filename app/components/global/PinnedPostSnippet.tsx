99914f191ff8b9c7e7c1c7872d96116c2ecd6dc6
zozzzC <czlc05edu@gmail.com>
2026-07-08 22:20:31 +1200

    fix: styling

--- app/components/global/PinnedPostSnippet.tsx
+++ app/components/global/PinnedPostSnippet.tsx
@@ -1,6 +1,6 @@
 export default async function PinnedPostSnippet() {
   return (
-    <div className="min-h-52 bg-white justify-center outline-black relative w-full outline-3">
+    <div className="min-h-52 bg-white justify-center outline-black relative w-full outline-3 lg:items-center">
       <div className="absolute w-15 h-5 bg-pop-blue -right-5 -bottom-1 -rotate-[25deg]"></div>
       <div className="absolute w-15 h-5 bg-pop-magenta -top-1 -left-5 -rotate-[18deg]"></div>
       <div className="flex flex-col justify-center h-full px-5">
