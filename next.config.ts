0f56d91affdec60db303c0dc100331e5b79c92aa
zozzzC <czlc05edu@gmail.com>
2026-07-13 22:51:35 +1200

    feat: add unsplash to remote patterns

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "makeuoa.nz",
         port: "",
         pathname: "/**",
