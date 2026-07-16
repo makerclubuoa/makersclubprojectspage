f1d5d694a21092c1203af574fdfcb38ad5adc24c
zozzzC <czlc05edu@gmail.com>
2026-07-16 12:37:44 +1200

    fix: text oveflow

--- app/components/homepage/Header.tsx
+++ app/components/homepage/Header.tsx
@@ -16,12 +16,12 @@ export default function Header({
 }: HeaderPropsType) {
   return (
     <div
-      className={`bg-${bgColour} text-${colour} ${typeOverride} flex items-center w-[110%] content-stretch outline-solid outline-3 outline-black [letter-spacing:0.1em] xl:p-12`}
+      className={`bg-${bgColour} text-${colour} ${typeOverride} flex items-center w-full md:w-[110%] content-stretch outline-solid outline-3 outline-black [letter-spacing:0.1em] xl:p-12`}
       style={{ transform: `rotate(${rotation}deg)` }}
     >
       <p
         className={`font-bold text-3xl md:text-4xl font-holt
-          text-shadow-lg [-webkit-text-stroke:6px_black] [paint-order:stroke_fill] ${textTypeOverride}`}
+            text-shadow-lg [-webkit-text-stroke:6px_black] [paint-order:stroke_fill] ${textTypeOverride} pr-5`}
       >
         {text}
       </p>
